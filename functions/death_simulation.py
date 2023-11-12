46a5896f3054810fcc952584353a4a2428f0be0a
SchumPau <paul.schumacher.dd@web.de>
2023-11-12 19:08:12 +0100

    Bug Removed

--- functions/death_simulation.py
+++ functions/death_simulation.py
@@ -1,13 +1,25 @@
 from classes.Creature import Creature
 from classes.World import World
 from classes.Food import Food
+from functions.functions import *
 
 def simulation():
+    lim_x = 10000
+    lim_y = 10000
+    
+    world = World(lim_x, lim_y)
     number_of_food = 10000
+    food_items = []
     
+    for i in range(number_of_food):
+        x_position = random.randint(lim_x)
+        y_position = random.randint(lim_y)
+        food_energy = random.randint(5,50)
+        food = Food(x_position,y_position,food_energy,1)
+        food_items.append(food)
+        
     food = Food(10,10,10,1)
-    
-    world = World(10000, 10000)
+        
     creature = Creature(5, 5, 10, 1, world, "A")
     
     while creature.energy > 0:
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from classes.Creature import Creature
 from classes.World import World
 from classes.Food import Food
-import def_simulation
 
-def_simulation.simulation()
 
