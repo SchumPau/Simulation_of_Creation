51bf624f65f3ac9982c9979ec1d60452253217aa
henrydatei <31316025+henrydatei@users.noreply.github.com>
2023-11-12 20:33:06 +0100

    list of creatures

--- classes/World.py
+++ classes/World.py
@@ -3,15 +3,18 @@ import dataclasses
 from typing import List
 
 from classes.Food import Food
+from classes.Creature import Creature
 
 @dataclasses.dataclass
 class World:
     max_x: int
     max_y: int
     food = List[Food]
+    creatures = List[Creature]
     
     def __post_init__(self):
         self.food = []
+        self.creatures = []
     
     def nearby_food(self, x, y, radius):
         nearby_food = [f for f in self.food if abs(f.x - x) <= radius and abs(f.y - y) <= radius]
--- functions/death_simulation.py
+++ functions/death_simulation.py
@@ -5,7 +5,6 @@ import random
 import logging
 import coloredlogs
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from matplotlib import style
 style.use('fivethirtyeight')
 
