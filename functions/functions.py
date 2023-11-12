c51565d24199bf91cb946df299fecf9d60f6f42f
SchumPau <paul.schumacher.dd@web.de>
2023-11-12 18:56:02 +0100

    Auslagerung Simulation

--- functions/functions.py
+++ functions/functions.py
@@ -3,4 +3,3 @@ import random
 def get_random_number():
     x = random.randint(1, 100)
     return x
-
--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 from classes.Creature import Creature
 from classes.World import World
 from classes.Food import Food
+import def_simulation
 
-number_of_food = 1000
+def_simulation.simulation()
 
-food = Food(10,10,10,1)
-
-world = World(10000, 10000)
-creature = Creature(5, 5, 10, 1, world, "A")
-
-while creature.energy > 0:
-    creature.move()
-    
-    for i in range(number_of_food):
-        if creature.x == food.x and creature.y == food.y:
-            creature.eat(food)
-            food = Food(10,10,0,1)
-            print("Food eaten")
-            break
-    
-    print(creature.x, creature.y, creature.energy)
