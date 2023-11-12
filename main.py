da095963e4763887b585ef67047b755067779150
SchumPau <paul.schumacher.dd@web.de>
2023-11-12 18:40:20 +0100

    Bugfix

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from classes.World import World
 
 world = World(10, 10)
 creature = Creature(5, 5, 10, 1, world, "A")
+
 while creature.energy > 0:
     creature.move()
     print(creature.x, creature.y, creature.energy)
