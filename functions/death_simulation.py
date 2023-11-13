8cba84ab693f030c11945440c96f04f650db5407
SchumPau <paul.schumacher.dd@web.de>
2023-11-13 18:50:13 +0100

    Comment and Added Next Generation

--- functions/death_simulation.py
+++ functions/death_simulation.py
@@ -4,79 +4,92 @@ from classes.Food import Food
 import random
 import logging
 import coloredlogs
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use('fivethirtyeight')
 
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='DEBUG', logger=logger, fmt='[%(asctime)s] %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-def simulation_plot(food_items,creature,world):
-    plt.scatter(creature.x, creature.y, s=100, c='red', marker='o')
-    plt.scatter([f.x for f in food_items], [f.y for f in food_items], s=50, c='green', marker='o')
-    plt.xlim(0, world.max_x)
-    plt.ylim(0, world.max_y)
-    plt.show()
 
-def simulation():
-    lim_x = 100
-    lim_y = 100
+def simulation(lim_x,lim_y,number_of_food,number_of_creature):
     
-    world = World(lim_x, lim_y)
-    number_of_food = 100
-    number_of_creature = 10
+    # Implementation variables
+    next_generation = []
     i = 0
-    
     creatures = []
+    dead = False
     
+    # Create world
+    world = World(lim_x, lim_y)
+    
+    # Creating Food    
     while len(world.food) < number_of_food:
         x_position = random.randint(0,lim_x)
         y_position = random.randint(0,lim_y)
-        food_energy = random.randint(5,50)
+        food_energy = random.randint(5,25)
         food = Food(x_position,y_position,food_energy)
-            
+        
+        # Listing of all Food and send it to world
         food_coordinates = [(f.x, f.y) for f in world.food]
+        
+        # Check for existing Food
         if (food.x, food.y) not in food_coordinates:
             world.food.append(food)
             logger.info(f"Food with X: {food.x} and Y: {food.y} and energy: {food.energy_provided} created!")
             i += 1
     
+    # Creating Creatures
     while len(creatures) < number_of_creature:
         x_position = random.randint(0,lim_x)
         y_position = random.randint(0,lim_y)
         creature_energy = random.randint(5,25)
         sensor_radius = random.randint(2,7)
         creature = Creature(x_position,y_position,creature_energy,sensor_radius,world,"A")
-            
+        
+        # Listing of all Creatures
         creature_coordinates = [(c.x, c.y) for c in creatures]
+        
+        # Check for existing Creatures
         if (creature.x, creature.y) not in creature_coordinates:
             creatures.append(creature)
             logger.info(f"Creature with X: {creature.x} and Y: {creature.y} and energy: {creature.energy} created!")
             i += 1
     
-    dead = False
-    
+    # Simulation
     while not dead:
+        # Shuffle of Creatures
         random.shuffle(creatures)
+        
         for creature in creatures:
             if creature.energy > 0:
                 creature.move()
+                
+                # Check for Food
                 for food in world.food:           
                     if creature.x == food.x and creature.y == food.y:
                         creature.eat(food)
                         world.food.remove(food)
-                        print("Food eaten")
+                        logger.info("Food eaten")
                         #break
+                
+                # Check for death and last creature
                 if creature.energy == 0 and len(creatures) == 1:
                     creature.death()
                     creatures.remove(creature)
-                    print("All Creatures died")
+                    next_generation.append(creature)
+                    logger.info("All Creatures died")
                     dead = True
-                elif creature.energy == 0:
-                    creature.death()
-                    creatures.remove(creature)
-                    print("Creature died")
                 
-                print(creature.x, creature.y, creature.energy)
+                # Check for death
+                elif creature.energy == 0:
+                    # Last three creatures will be added to next generation
+                    if len(creatures) <= 2:
+                        next_generation.append(creature)
+                    
+                    # Death of Creature
+                    creature.death()
+                    creatures.remove(creature)                    
+                    logger.info("Creature died")
+                
+                # Debug actual position and energy
+                logger.info(creature.x, creature.y, creature.energy)
     
-    return dead
+    return next_generation
--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from functions.death_simulation import simulation
 
 i = 0
 dead = False
+next_generation = []
+
 while not dead:
-    dead = simulation()
+    
+    next_generation = simulation(lim_x=100,lim_y=100,number_of_food=100,number_of_creature=10)
     i += 1
     
 print(f"Simulation finished after {i} iterations")
